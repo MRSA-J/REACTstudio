9dbfa0d953653719e75ce739aad92d8cbd28c733
ChenxiwuB <114111547+ChenxiwuB@users.noreply.github.com>
2022-11-10 01:02:47 -0500

    Update BakeryItem.js

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,17 @@
 // TODO: create a component that displays a single bakery item
+
+export default function Batem(a) {
+    return (
+        <div class="col-md-6 col-lg-3">
+            <div class="card h-100">
+            <img src={a.image} class="card-img-top" alt={'unknown'}/>
+                <div class="card-body">
+                    <h3 class="card-title">{a.name}</h3>
+                    <h5 class="card-text">{a.description}</h5>
+                    <h4 class="card-text">${a.price}</h4>
+                    <button class="btn btn-outline-success" onClick={() => a.handleClick(a)}>Add to Cart</button>
+                </div>
+            </div>
+        </div>
+    );
+}
