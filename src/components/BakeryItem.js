6e980c53aeeebd3cc240232a313e05b576b46929
MRSA-J <54956912+MRSA-J@users.noreply.github.com>
2022-11-10 15:46:49 -0500

    update bakery item components

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,17 +1,20 @@
 // TODO: create a component that displays a single bakery item
 
-export default function Batem(a) {
+export default function Batem(item) {
     return (
-        <div class="col-md-6 col-lg-3">
+        <div class="col-md-5 col-lg-4">
             <div class="card h-100">
-            <img src={a.image} class="card-img-top" alt={'unknown'}/>
+                <img src={item.image} class="card-img-top" alt={'unknown'}/>
                 <div class="card-body">
-                    <h3 class="card-title">{a.name}</h3>
-                    <h5 class="card-text">{a.description}</h5>
-                    <h4 class="card-text">${a.price}</h4>
-                    <button class="btn btn-outline-success" onClick={() => a.handleClick(a)}>Add to Cart</button>
+                    <h3 class="card-title">{item.name}</h3>
+                    <text class="card-text">{item.description}</text>
+                    <div class="flex items-center justify-between">
+                        <h5 class="card-text">${item.price}</h5>
+                        <button class="btn btn-outline-success" onClick={() => item.handleClick(item)}>Add to Cart</button>
+                    </div>
                 </div>
             </div>
         </div>
     );
 }
+
